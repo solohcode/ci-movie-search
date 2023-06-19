c477659e8b00b94ae5fb4cfb5c64b7f934f95668
solohcode <solomonayodele65@gmail.com>
2023-06-19 13:12:42 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
+	<React.Fragment>
     <QueryClientProvider client={queryClient}>
       <Home />
     </QueryClientProvider>
-	</React.StrictMode>
+	</React.Fragment>
 );
 
 // If you want to start measuring performance in your app, pass a function
